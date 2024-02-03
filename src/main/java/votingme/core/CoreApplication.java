af583ae003445ea1eacdfac3eaac9d9616f0b573
timibacco <48661125+timibacco@users.noreply.github.com>
2024-02-03 15:50:56 +0100

    Update CoreApplication.java

--- src/main/java/votingme/core/CoreApplication.java
+++ src/main/java/votingme/core/CoreApplication.java
@@ -2,7 +2,7 @@ package votingme.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class CoreApplication {
